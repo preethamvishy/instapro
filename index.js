8abb5b35c96a0101a498b610fedb825821c1a10f
Preetham Viswanathan <preethamvishy10@gmail.com>
2018-04-09 23:29:14 +0530

    Cookie added for Instagram graphql queries

--- index.js
+++ index.js
@@ -1,46 +1,52 @@
 const fetch = require('isomorphic-fetch')
+var cookie = 'ig_pr=2'
 
 exports.getUserByUsername = username => (
     fetch(`https://www.instagram.com/${username}/?__a=1`)
         .then(res => res.json())
         .catch(err => {
-			err.message = `"${username}" not found`;
-			throw err;
-		})
+            err.message = `"${username}" not found`;
+            throw err;
+        })
 )
 
 exports.getUserIdFromUsername = username => (
     fetch(`https://www.instagram.com/${username}/?__a=1`)
         .then(res => res.json()
-        .then(({user}) => { 
-                        return { id: user.id } 
-                    })
-            )
+            .then(({ user }) => {
+                return { id: user.id }
+            })
         )
+)
 
 exports.getMediaByCode = shortcode => (
     fetch(`https://www.instagram.com/p/${shortcode}/?__a=1`)
         .then(res => res.json()
-        .then(({graphql}) => graphql)
-    )
+            .then(({ graphql }) => graphql)
+        )
 )
 
 exports.getMediaLikesByCode = (
     shortcode,
     count,
     commentId = '') => (
-        fetch(`https://www.instagram.com/graphql/query/?query_id=17864450716183058&shortcode=${shortcode}&first=${count}&after=${commentId}`)
+
+        fetch(`https://www.instagram.com/graphql/query/?query_id=17864450716183058&shortcode=${shortcode}&first=${count}&after=${commentId}`, {
+            headers: { Cookie: cookie }
+        })
             .then(res => res.json())
-            .then(({data}) => data.shortcode_media)
+            .then(({ data }) => data.shortcode_media)
     )
 
 exports.getMediaCommentsByCode = (
     shortcode,
     count,
     commentId = '') => (
-        fetch(`https://www.instagram.com/graphql/query/?query_id=17852405266163336&shortcode=${shortcode}&first=${count}&after=${commentId}`)
+        fetch(`https://www.instagram.com/graphql/query/?query_id=17852405266163336&shortcode=${shortcode}&first=${count}&after=${commentId}`, {
+            headers: { Cookie: cookie }
+        })
             .then(res => res.json())
-            .then(({data}) => data.shortcode_media.edge_media_to_comment)
+            .then(({ data }) => data.shortcode_media.edge_media_to_comment)
     )
 
 exports.getMediaByLocation = (
@@ -55,14 +61,16 @@ exports.getMediaByTag = (
     maxId = '') => (
         fetch(`https://www.instagram.com/explore/tags/${tag}/?__a=1&max_id=${maxId}`)
             .then(res => res.json())
-            .then(({graphql}) => graphql)
+            .then(({ graphql }) => graphql)
     )
 
 exports.getUserMediaAdvanced = (
     userId,
     count = 50,
     after = '') => (
-        fetch(`https://www.instagram.com/graphql/query/?query_id=17888483320059182&id=${userId}&first=${count}&after=${after}`)
+        fetch(`https://www.instagram.com/graphql/query/?query_id=17888483320059182&id=${userId}&first=${count}&after=${after}`, {
+            headers: { Cookie: cookie }
+        })
             .then(res => res.json())
     )
 
@@ -70,18 +78,22 @@ exports.getUserFollowers = (
     userId,
     count = 50,
     after = '') => (
-        fetch(`https://www.instagram.com/graphql/query/?query_id=17851374694183129&id=${userId}&first=${count}&after=${after}`)
+        fetch(`https://www.instagram.com/graphql/query/?query_id=17851374694183129&id=${userId}&first=${count}&after=${after}`, {
+            headers: { Cookie: cookie }
+        })
             .then(res => res.json())
-            .then(({data}) => data.user)
+            .then(({ data }) => data.user)
     )
 
 exports.getUserFollowing = (
     userId,
     count = 50,
     after = '') => (
-        fetch(`https://www.instagram.com/graphql/query/?query_id=17874545323001329&id=${userId}&first=${count}&after=${after}`)
+        fetch(`https://www.instagram.com/graphql/query/?query_id=17874545323001329&id=${userId}&first=${count}&after=${after}`, {
+            headers: { Cookie: cookie }
+        })
             .then(res => res.json())
-            .then(({data}) => data.user)            
+            .then(({ data }) => data.user)
     )
 exports.generalSearch = (
     query) => (
@@ -92,5 +104,5 @@ exports.generalSearch = (
 exports.getUserProfilePicture = username => (
     fetch(`https://www.instagram.com/${username}/?__a=1`)
         .then(res => res.json())
-        .then(({user}) => user.profile_pic_url_hd)        
+        .then(({ user }) => user.profile_pic_url_hd)
 )
